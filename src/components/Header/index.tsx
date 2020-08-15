a736d83014893386023c3399783b603c4cf0ec3b
Levi Siebra <levils114@gmail.com>
2020-08-15 18:05:52 -0300

    challenge finished

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { FiPlusSquare } from 'react-icons/fi';
+import { Container } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+import ModalAddFood from './../ModalAddFood/';
+
+interface IHeaderProps {
+  openModal: () => void;
+}
+
+const Header: React.FC<IHeaderProps> = ({ openModal }) => (
+  <Container>
+    <header>
+      <img src={Logo} alt="GoRestaurant" />
+      <nav>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              openModal()
+            }}
+          >
+            <div className="text">Novo Prato</div>
+            <div className="icon">
+              <FiPlusSquare size={24} />
+            </div>
+          </button>
+        </div>
+      </nav>
+    </header>
+  </Container>
+);
+
+export default Header;
